76e7d138258ab08fe19aa75d221eb216997b2239
martinecececek <104137335+martinecececek@users.noreply.github.com>
2024-01-14 12:48:05 +0100

    added new inheritance class for better look

---
+++ MainForm.cs
@@ -0,0 +1,12 @@
+using MaterialSkin.Controls;
+
+namespace SortifyDB
+{
+    public partial class MainForm : MaterialForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
