4f4cdd972450419020c07cafad63bf6e66e9093a
Volchar <144711292+Volchar-CZ@users.noreply.github.com>
2024-01-14 17:21:40 +0100

    Snad fix

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SortifyDB.PropertiesA {
+namespace SortifyDB.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SortifyDB.PropertiesA {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SortifyDB.PropertiesA.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SortifyDB.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
