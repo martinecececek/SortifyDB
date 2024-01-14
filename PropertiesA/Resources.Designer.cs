2e2cfcba749258451e31c2535a00ce21e5d2d03a
Volchar <144711292+Volchar-CZ@users.noreply.github.com>
2024-01-14 17:21:13 +0100

    Doing fixing

--- PropertiesA/Resources.Designer.cs
+++ PropertiesA/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SortifyDB.Properties {
+namespace SortifyDB.PropertiesA {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SortifyDB.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SortifyDB.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SortifyDB.PropertiesA.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
