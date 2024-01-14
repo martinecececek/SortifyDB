557b20506ffbda0dc39112ef935e9330182b470a
Volchar <144711292+Volchar-CZ@users.noreply.github.com>
2024-01-14 16:34:48 +0100

    Made the UI
    Created, tweaked the UI.

---
+++ MainUserControl.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortifyDB
+{
+    public partial class MainUserControl : UserControl
+    {
+        public MainUserControl()
+        {
+            InitializeComponent();
+        }
+        private void btnProjekty_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMat_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCleanActive_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVarnish_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGran_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMainFormsAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnParser_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
