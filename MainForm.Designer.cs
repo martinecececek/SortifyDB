ba7e3143ff1e731f6ddce20d1515083a18942113
martinecececek <104137335+martinecececek@users.noreply.github.com>
2024-01-14 16:50:34 +0100

    reverted

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,32 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            mainUserControl1 = new MainUserControl();
             SuspendLayout();
-            // 
-            // mainUserControl1
-            // 
-            mainUserControl1.BackColor = Color.Lavender;
-            mainUserControl1.Location = new Point(1, 64);
-            mainUserControl1.Name = "mainUserControl1";
-            mainUserControl1.Size = new Size(1920, 1015);
-            mainUserControl1.TabIndex = 0;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1920, 1080);
-            Controls.Add(mainUserControl1);
-            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "MainForm";
-            Text = "SortifyDB";
+            Text = "Form1";
             ResumeLayout(false);
         }
 
         #endregion
-
-        private MainUserControl mainUserControl1;
     }
 }
