513b9d03b1c829a25c276eafc57fd23e41314652
martinecececek <104137335+martinecececek@users.noreply.github.com>
2024-01-15 18:24:14 +0100

    dummy data corected

--- MainForm.cs
+++ MainForm.cs
@@ -6,13 +6,14 @@ namespace SortifyDB
     public partial class MainForm : MaterialForm
     {
         //create lists of objects from sheets
-        public static List<Material> Materials { get; set; }
-        public static List<KluzkyLak> KluzkeLaky { get; set; }
-        public static List<Granulat> Granulaty { get; set; }
-        public static List<CisiticAktivator> CisticeAktivatory { get; set; }
-        public static List<Projekt> Projekty { get; set; }
+        public static List<Material> Materials { get; set; } = [];
+        public static List<KluzkyLak> KluzkeLaky { get; set; } = [];
+        public static List<Granulat> Granulaty { get; set; } = [];
+        public static List<CisiticAktivator> CisticeAktivatory { get; set; } = [];
+        public static List<Projekt> Projekty { get; set; } = [];
 
-        public static List<Ms_Todo.TodoTask> TodoTask { get; set; }
+        public static List<Ms_Todo.TodoTask> TodoTask { get; set; } = [];
+
 
         public MainForm()
         {
